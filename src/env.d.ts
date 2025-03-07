ef2fc9c051ffca7cb12cb5c2c7db13de6c1fe662
Dawam Raja <akunnyadawam9@gmail.com>
2025-03-07 16:28:52 +0700

    ✨ (src/env.d.ts): add environment variables and runtime type definitions for AstroJS Cloudflare deployment

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,14 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+type ENV = {
+  // replace `MY_KV` with your KV namespace
+  API_URL: string;
+};
+
+// use a default runtime configuration (advanced mode).
+type Runtime = import('@astrojs/cloudflare').Runtime<ENV>;
+declare namespace App {
+  interface Locals extends Runtime {}
+}
